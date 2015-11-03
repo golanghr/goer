e2e7e8bab558cabbbbe755b3ca63e330fdbef441
Davor Kapsa <davorkapsa@gmail.com>
2015-11-03 17:06:05 +0100

    registration: Clean TestFormDisplay

--- registration_test.go
+++ registration_test.go
@@ -42,11 +42,11 @@ func TestFormDisplay(t *testing.T) {
 	reg.ServeHTTP(resp, req)
 
 	if resp.Code != http.StatusOK {
-		t.Fatal(fmt.Sprintf("Server repled with status %v expected %v", resp.Code, http.StatusOK))
+		t.Fatalf("Server replied with status %v expected %v", resp.Code, http.StatusOK)
 	}
 
 	if !strings.Contains(resp.Body.String(), "<form") {
-		t.Fatal("Registraton dose not caontain form element!")
+		t.Fatal("Registration does not contain form element!")
 	}
 }
 
