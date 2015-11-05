d0f4511489f9ab6399a33897056cd21eeb8ff410
Davor Kapsa <davorkapsa@gmail.com>
2015-11-05 10:24:21 +0100

    content: Clean TestHTML

--- lib/content/content_test.go
+++ lib/content/content_test.go
@@ -1,33 +1,36 @@
 package content
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 )
 
-func TestParseMkToHTML(t *testing.T) {
+func TestHTML(t *testing.T) {
 	r := strings.NewReader(`
 # Main title
 
 ## Subtitle
 
-This is some text with a [link](https://www.google.com).
+This is some text with a [link](https://www.golang.hr).
 `)
 
 	html, err := HTML(r)
 
 	if err != nil {
-		t.Fatal(fmt.Sprintf("Content parsing failed with error %s", err))
+		t.Fatalf("Content parsing failed with error %s", err)
 	}
 
-	if !strings.Contains(html, "<h1>Main title</h1>") {
-		t.Fatal(fmt.Sprintf("Expected \"%s\" not recived in \"%s\"", "<h1>Main title</h1>", html))
+	tests := []struct {
+		want string
+	}{
+		{"<h1>Main title</h1>"},
+		{"<h2>Subtitle</h2>"},
+		{"<a href=\"https://www.golang.hr\">link</a>"},
 	}
-	if !strings.Contains(html, "<h2>Subtitle</h2>") {
-		t.Fatal(fmt.Sprintf("Expected \"%s\" not recived in \"%s\"", "<h2>Subtitle</h2>", html))
-	}
-	if !strings.Contains(html, "<a href=\"https://www.google.com\">link</a>") {
-		t.Fatal(fmt.Sprintf("Expected \"%s\" not recived in \"%s\"", "<a href=\"https://www.google.com\">link</a>", html))
+
+	for _, tt := range tests {
+		if !strings.Contains(html, tt.want) {
+			t.Errorf("Expected %q not found", tt.want)
+		}
 	}
 }
